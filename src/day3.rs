b524cd5184d063d9aa906abcde89921165e5576e
orel <oreltop@gmail.com>
2025-05-03 13:36:34 +0300

    finishing day3 part2

--- src/day3.rs
+++ src/day3.rs
@@ -2,16 +2,40 @@ use regex::Regex;
 use std::fs;
 
 const FILE_NAME: &'static str = "input_day3.txt";
-const REGEX: &str = r"mul\((\d+),(\d+)\)";
+const REGEX_PART_1: &str = r"mul\((\d+),(\d+)\)";
+const REGEX_PART_2: &str = r"mul\((\d+),(\d+)\)|don't|do";
+
 pub fn main() {
     println!("{}", "this is main");
     let file_path = format!("artifacts/{}", FILE_NAME);
     let input = fs::read_to_string(file_path).expect("Should have been able to read the file");
-    let result: i32 = find_regex_in_str(REGEX, &input)
+    main_part2(&input);
+}
+
+fn main_part1(input: &str){
+    let result: i32 = find_regex_in_str(REGEX_PART_1, input)
         .iter()
         .map(|expression| parse_mul_arg(expression))
         .sum();
     println!("{}", result);
+}
+
+fn main_part2(input: &str){
+    let mut active = true;
+    
+    let matches = find_regex_in_str(REGEX_PART_2,input);
+    let mut result = 0;
+    for item in matches{
+        match item{
+            "do" => active = true,
+            "don't" => active = false,
+            _ => if active{
+                result += parse_mul_arg(item)
+            } else {  }
+        }
+    }
+    println!("{}", result)
+    
 }
 
 fn parse_mul_arg(input: &str) -> i32 {
@@ -50,12 +74,26 @@ pub mod tests {
         assert_eq!(matches, vec!["quick", "brown", "jumps"]);
     }
     #[test]
-    fn test_find_regex() {
+    fn test_find_regex_part1() {
         let text = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
-        let matches = find_regex_in_str(REGEX, text);
+        let matches = find_regex_in_str(REGEX_PART_1, text);
         assert_eq!(
             matches,
             vec!["mul(2,4)", "mul(5,5)", "mul(11,8)", "mul(8,5)"]
         );
     }
+    #[test]
+    fn test_find_regex_part2(){
+        let text = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        assert_eq!(
+            find_regex_in_str(REGEX_PART_2, text),
+            vec!["mul(2,4)", "do", "mul(5,5)", "mul(11,8)", "mul(8,5)"]
+        );
+        let text = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        assert_eq!(
+            find_regex_in_str(REGEX_PART_2, text),
+            vec!["mul(2,4)", "don't", "mul(5,5)", "mul(11,8)", "do", "mul(8,5)"]
+        );
+    }
+    
 }
