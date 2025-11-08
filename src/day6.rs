d8496a9c5c8fac9ab77408036112e96677cd558b
oreltop <oreltop@gmail.com>
2025-11-08 07:47:38 +0200

    green

--- src/day6.rs
+++ src/day6.rs
@@ -1,55 +1,128 @@
+use crate::day6::Direction::{Down, Left, Right, Up};
 use std::fs;
 
 const FILE_NAME: &str = "input_day1.txt";
 
-#[derive(Debug)]
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 enum Direction {
     Up,
     Right,
     Down,
-    Left
+    Left,
 }
 
-struct Position{
+#[derive(Debug, PartialEq)]
+struct Size {
+    rows: usize,
+    columns: usize,
+}
+#[derive(Debug, PartialEq)]
+struct Position {
     x: usize,
-    y: usize
+    y: usize,
 }
+#[derive(Debug, PartialEq)]
 
-enum Cell{
+enum Cell {
     Empty,
     Obstruction,
+    InitialGuardPosition(Direction),
 }
 
 impl Cell {
     fn is_empty(&self) -> bool {
         match self {
             Cell::Empty => true,
+            Cell::InitialGuardPosition(_) => true,
             Cell::Obstruction => false,
         }
     }
 }
 
-struct Guard{
+struct Guard {
     position: Position,
-    direction: Direction
+    direction: Direction,
 }
 
-struct World{
-    size: (usize, usize),
+struct World {
+    size: Size,
     map: Vec<Vec<Cell>>,
     frame: usize,
-    guard: Guard
+    guard: Guard,
 }
 impl World {
-    fn from(input: &str) -> World{
-        let input_lines = input.trim().lines();
-
+    fn from(input: &str) -> World {
+        let input_lines: Vec<&str> = input.trim().lines().collect();
+        let size = (input_lines.len(), input_lines[0].len());
+        // let map: = input_lines.iter().map(
+        //     // |line| line.ma
+        // )
         todo!()
     }
 }
 
+struct WorldBuilder();
+impl WorldBuilder {
+    fn build(input: &str) -> World {
+        let size = Self::get_size(input);
+        let map = Self::build_map(input);
+        let guard = Self::build_guard(&map);
+        World {
+            size,
+            map,
+            frame: 0,
+            guard,
+        }
+    }
 
+    fn get_size(input: &str) -> Size {
+        let input_lines: Vec<&str> = input.lines().collect();
+        Size {
+            rows: input_lines.len(),
+            columns: input_lines[0].len(),
+        }
+    }
+
+    fn build_line(line: &str) -> Vec<Cell> {
+        line.chars()
+            .map(|c| match c {
+                '.' => Cell::Empty,
+                '^' => Cell::InitialGuardPosition(Up),
+                '>' => Cell::InitialGuardPosition(Right),
+                '<' => Cell::InitialGuardPosition(Left),
+                'V' => Cell::InitialGuardPosition(Down),
+                '#' => Cell::Obstruction,
+                _ => panic!("invalid character!"),
+            })
+            .collect()
+    }
+    fn build_map(input: &str) -> Vec<Vec<Cell>> {
+        input
+            .lines()
+            .map(|line| WorldBuilder::build_line(line))
+            .collect()
+    }
+    fn build_guard(map: &[Vec<Cell>]) -> Guard {
+        let (x, y) = map
+            .iter()
+            .enumerate()
+            .find_map(|(x, row)| {
+                row.iter()
+                    .enumerate()
+                    .find(|(y, cell)| matches!(cell, Cell::InitialGuardPosition(_)))
+                    .map(|(y, _)| (x, y))
+            })
+            .unwrap();
+        let Cell::InitialGuardPosition(direction) = map[x][y] else {
+            panic!("this shouldn't happen")
+        };
+
+        Guard {
+            position: Position { x, y },
+            direction,
+        }
+    }
+}
 
 pub fn main() {
     println!("this is main");
@@ -58,7 +131,6 @@ pub fn main() {
     // println!("input: {:?}", input);
     // let parsed = parse_string(&input);
     // println!("input parsed: {:?}", &parsed);
-
 }
 
 fn parse_string(input: &str) -> Vec<i32> {
@@ -66,24 +138,27 @@ fn parse_string(input: &str) -> Vec<i32> {
     column1
 }
 
-
 #[cfg(test)]
 pub mod tests {
     use super::*;
     #[test]
-    fn new_from_input(){
+    fn new_from_input() {
         let input = r"
 ...^
 ####
 ....
-....";
-       let world = World::from(input);
-        assert_eq!(world.size, (4,4));
-        assert_eq!(world.guard.position.x,0);
-        assert_eq!(world.guard.position.y,3);
+";
+        let world = WorldBuilder::build(input.trim());
+        assert_eq!(
+            world.size,
+            Size {
+                rows: 3,
+                columns: 4
+            }
+        );
+        assert_eq!(world.guard.position, Position { x: 0, y: 3 });
         assert_eq!(world.guard.direction, Direction::Up);
         assert!(!world.map[1][3].is_empty());
         assert!(world.map[0][3].is_empty());
-
     }
 }
