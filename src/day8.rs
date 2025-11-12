c62a141998995165ca8705cfc3c88a97ae218ee7
oreltop <oreltop@gmail.com>
2025-11-12 15:10:22 +0200

    green

--- src/day8.rs
+++ src/day8.rs
@@ -16,8 +16,22 @@ pub fn main() {
 struct Point(i32, i32);
 
 fn parse_string(input: &str) -> HashMap<char, Vec<Point>> {
-    let mut column1: Vec<i32> = Vec::new();
-    column1
+    input
+        .trim()
+        .lines()
+        .rev()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.trim()
+                .chars()
+                .enumerate()
+                .filter(|(x, c)| c != &'.')
+                .map(move |(x, c)| (c, Point(x as i32, y as i32)))
+        })
+        .fold(HashMap::new(), |mut antennas, (c, point)| {
+            antennas.entry(c).or_default().push(point);
+            antennas
+        })
 }
 
 #[cfg(test)]
@@ -41,6 +55,7 @@ pub mod tests {
         ............";
 
         let mut parsed = parse_string(input);
+        println!("{:?}", parsed);
 
         let values_0 = parsed.get(&'0').unwrap();
         let result_0 = vec![Point(4, 7), Point(7, 8), Point(5, 9), Point(8, 10)];
@@ -48,8 +63,8 @@ pub mod tests {
         assert_eq!(values_0.len(), result_0.len());
 
         let values_A = parsed.get(&'A').unwrap();
-        let result_A = vec![Point(2, 9), Point(3, 8), Point(6, 6)];
-        assert!(result_A.iter().all(|p| values_0.iter().contains(p)));
-        assert_eq!(values_A.len(), result_0.len());
+        let result_A = vec![Point(9, 2), Point(8, 3), Point(6, 6)];
+        assert!(result_A.iter().all(|p| values_A.iter().contains(p)));
+        assert_eq!(values_A.len(), result_A.len());
     }
 }
