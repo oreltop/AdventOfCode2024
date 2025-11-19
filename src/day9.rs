602dbdfb9b43fca79d8aae01f79e0c5a2590e9b4
oreltop <oreltop@gmail.com>
2025-11-19 08:37:58 +0200

    red, still red, but at least compiles

--- src/day9.rs
+++ src/day9.rs
@@ -1,6 +1,10 @@
+use itertools::{Itertools, concat};
 use std::fs;
+use std::iter::once;
+use std::thread::yield_now;
 
 const FILE_NAME: &str = "input_day9.txt";
+const EMPTY_SPACE: i32 = -1;
 
 pub fn main() {
     println!("this is main");
@@ -35,10 +39,26 @@ pub fn main() {
 //     todo!()
 // }
 
-fn parse_string(s: &str) -> Vec<i32>{
-
-
-    todo!()
+fn parse_string(s: &str) -> Vec<i32> {
+    let pairs: Vec<(i32, i32)> = s
+        .chars()
+        .into_iter()
+        .map(|c| c.to_digit(10).unwrap() as i32)
+        .chain(once(0)) // to avoid the last item being dropped if odd
+        .tuples()
+        .collect();
+    println!("{:?}", pairs);
+    pairs
+        .iter()
+        .enumerate()
+        .flat_map(|(index, (block_size, free_space))| {
+            [
+                vec![index as i32; *block_size as usize],
+                vec![EMPTY_SPACE; *free_space as usize],
+            ]
+            .concat()
+        })
+        .collect()
 }
 
 #[cfg(test)]
@@ -46,9 +66,9 @@ pub mod tests {
     use super::*;
 
     #[test]
-    fn test_parse_string(){
+    fn test_parse_string() {
         let string = "143023";
-        let parsed = vec![0,-1-1-1-1,1,1,1,2,2,-1-1-1];
+        let parsed = vec![0, -1 - 1 - 1 - 1, 1, 1, 1, 2, 2, -1 - 1 - 1];
         assert_eq!(parse_string(string), parsed);
     }
 
