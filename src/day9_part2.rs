f2736675cb848814099894feccce0eb7c3702b45
oreltop <oreltop@gmail.com>
2025-12-04 22:51:23 +0200

    green

--- src/day9_part2.rs
+++ src/day9_part2.rs
@@ -1,6 +1,9 @@
 const FILE_NAME: &str = "input_day9.txt";
+const EMPTY_SPACE: i32 = -1;
+
+use itertools::Itertools;
 use std::fs;
-use std::iter::Map;
+use std::iter::once;
 
 struct Disk {
     space: Vec<i32>,
@@ -8,10 +11,29 @@ struct Disk {
 
 impl Disk {
     fn new(input: &str) -> Disk {
-        todo!()
+        Disk {
+            space: Disk::parse_string(input),
+        }
     }
-    fn present(&self) -> String {
-        todo!()
+
+    fn parse_string(s: &str) -> Vec<i32> {
+        let pairs: Vec<(i32, i32)> = s
+            .chars()
+            .map(|c| c.to_digit(10).unwrap() as i32)
+            .chain(once(0)) // to avoid the last item being dropped if odd
+            .tuples()
+            .collect();
+        pairs
+            .iter()
+            .enumerate()
+            .flat_map(|(index, (block_size, free_space))| {
+                [
+                    vec![index as i32; *block_size as usize],
+                    vec![EMPTY_SPACE; *free_space as usize],
+                ]
+                .concat()
+            })
+            .collect()
     }
 }
 
@@ -30,8 +52,8 @@ pub mod tests {
     #[test]
     fn test_parse_string() {
         let string = "143023";
-        let expected = "0....11122...";
-        let result = Disk::new(string).present();
+        let expected = vec![0, -1, -1, -1, -1, 1, 1, 1, 2, 2, -1, -1, -1];
+        let result = Disk::new(string).space;
         assert_eq!(expected, result);
     }
 }
