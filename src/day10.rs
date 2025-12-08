9e8b669b6c987ef651c6e51a4f6c8ccd70f32d9c
oreltop <oreltop@gmail.com>
2025-12-08 15:29:41 +0200

    get value

--- src/day10.rs
+++ src/day10.rs
@@ -1,26 +1,46 @@
-use std::fs;
-
-const FILE_NAME: &str = "input_day1.txt";
+use std::sync::OnceLock;
+const INPUT: &str = include_str!("../artifacts/input_files/input_day10.txt");
+static GRID: OnceLock<Vec<Vec<u32>>> = OnceLock::new();
 
 pub fn main() {
     println!("this is main");
-    let file_path = format!("artifacts/input_files/{}", FILE_NAME);
-    let input = fs::read_to_string(file_path).expect("Should have been able to read the file");
-    println!("input: {:?}", input);
-    let parsed = parse_string(&input);
-    println!("input parsed: {:?}", &parsed);
+    let grid = get_grid();
 }
 
-fn parse_string(input: &str) -> Vec<i32> {
-    let mut column1: Vec<i32> = Vec::new();
-    column1
+fn get_grid() -> &'static Vec<Vec<u32>> {
+    GRID.get_or_init(|| {
+        INPUT
+            .split_whitespace()
+            .map(|str| str.chars().map(|c| c.to_digit(10).unwrap()).collect())
+            .collect()
+    })
+}
+fn get_value(x:usize, y:usize)->u32{
+    get_grid()[y][x]
+}
+
+struct Cell{
+    x: usize,
+    y: usize,
+    value: u32
+}
+
+impl Cell{
+    fn new(x: usize, y:usize) -> Cell{
+        Cell{x,y, value:get_value(x,y)}
+    }
 }
 
 #[cfg(test)]
 pub mod tests {
     use super::*;
     #[test]
-    fn test_parse_string() {
-        println!("{}", "this is test dummy")
+    fn new_cell() {
+        let cell = Cell::new(0,0);
+        assert_eq!(cell.value,5);
+        let cell = Cell::new(1,0);
+        assert_eq!(cell.value,6);
+        let cell = Cell::new(0,1);
+        assert_eq!(cell.value,4);
     }
 }
