de61df7b06b845ba9e0e4d4b8109cba793aef901
oreltop <oreltop@gmail.com>
2025-12-31 13:52:06 +0200

    green

--- src/day13.rs
+++ src/day13.rs
@@ -1,5 +1,5 @@
 use itertools::Itertools;
-use nalgebra::{DMatrix, DVector};
+use nalgebra::{ComplexField, Matrix2, Vector2};
 use std::fs;
 
 const FILE_NAME: &str = "input_day13.txt";
@@ -19,18 +19,20 @@ enum NumberOfSolutions {
 }
 
 struct ClawMachine {
-    movement_matrix: DMatrix<u32>,
-    target: DVector<u32>,
+    movement_matrix: Matrix2<f32>,
+    target: Vector2<f32>,
 }
+
 impl ClawMachine {
-    fn new(a_movement: (u32, u32), b_movement: (u32, u32), target: (u32, u32)) -> ClawMachine {
+    fn new(a_movement: (f32, f32), b_movement: (f32, f32), target: (f32, f32)) -> ClawMachine {
         ClawMachine {
-            movement_matrix: DMatrix::from_row_slice(
-                2,
-                2,
-                &[a_movement.0, b_movement.0, a_movement.1, b_movement.1],
-            ),
-            target: DVector::from_row_slice(&[target.0, target.1]),
+            movement_matrix: Matrix2::from_row_slice(&[
+                a_movement.0,
+                b_movement.0,
+                a_movement.1,
+                b_movement.1,
+            ]),
+            target: Vector2::from_row_slice(&[target.0, target.1]),
         }
     }
 
@@ -43,7 +45,7 @@ impl ClawMachine {
         ClawMachine::new(a_movement, b_movement, target)
     }
 
-    fn parse_line(line: Option<&str>, pattern: &str) -> (u32, u32) {
+    fn parse_line(line: Option<&str>, pattern: &str) -> (f32, f32) {
         let s = line.expect("missing line");
         let mut p = pattern.split("{d}");
         let (pre, mid) = (p.next().unwrap(), p.next().unwrap());
@@ -54,13 +56,36 @@ impl ClawMachine {
             .and_then(|s| s.split_once(mid))
             .expect("pattern mismatch");
         (
-            n1.parse().expect("invalid u32"),
-            n2.parse().expect("invalid u32"),
+            n1.parse().expect("invalid f32"),
+            n2.parse().expect("invalid f32"),
         )
     }
 
     fn number_of_solutions(&self) -> NumberOfSolutions {
-        todo!()
+        let option_solution = self.movement_matrix.clone().lu().solve(&self.target);
+        match option_solution {
+            None => {
+                let cramer_matrix =
+                    Matrix2::from_columns(&[self.movement_matrix.column(0), self.target.column(0)]);
+                if cramer_matrix.determinant().abs() < 1e-7 {
+                    NumberOfSolutions::Infinity
+                } else {
+                    NumberOfSolutions::None
+                }
+            }
+            Some(solution) => {
+                if ClawMachine::is_vector_natural(solution) {
+                    NumberOfSolutions::One
+                } else {
+                    NumberOfSolutions::None
+                }
+            }
+        }
+    }
+
+    fn is_vector_natural(v: Vector2<f32>) -> bool {
+        v.iter()
+            .all(|element| element.fract() == 0.0 && element >= &0.0)
     }
 }
 
@@ -83,12 +108,12 @@ pub mod tests {
 
         let machine = ClawMachine::from(s);
 
-        assert_eq!(machine.movement_matrix[(0, 0)], 94);
-        assert_eq!(machine.movement_matrix[(0, 1)], 22);
-        assert_eq!(machine.movement_matrix[(1, 0)], 34);
-        assert_eq!(machine.movement_matrix[(1, 1)], 67);
-        assert_eq!(machine.target[0], 8400);
-        assert_eq!(machine.target[1], 5400);
+        assert_eq!(machine.movement_matrix[(0, 0)], 94.0);
+        assert_eq!(machine.movement_matrix[(0, 1)], 22.0);
+        assert_eq!(machine.movement_matrix[(1, 0)], 34.0);
+        assert_eq!(machine.movement_matrix[(1, 1)], 67.0);
+        assert_eq!(machine.target[0], 8400.0);
+        assert_eq!(machine.target[1], 5400.0);
     }
 
     #[test]
@@ -99,7 +124,7 @@ pub mod tests {
         assert_eq!(parsed.len(), 320);
     }
     #[test]
-    fn number_of_solutions() {
+    fn single_solution() {
         let s = r"
         Button A: X+94, Y+34
         Button B: X+22, Y+67
